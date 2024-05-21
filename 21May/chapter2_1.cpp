b880efa9a6180addb41580ede1ebbe2598a9aa18
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-05-21 09:58:02 +0530

    21MayProblems

---
+++ 21May/chapter2_1.cpp
@@ -0,0 +1,12 @@
+/**write a program to display the following output using a single cout statement.
+ * Maths = 90
+ * Physics = 77
+ * Chemistry = 78
+*/
+#include <iostream>
+using namespace std;
+int main(){
+
+    cout <<"Maths = 90" <<"\nPhysics = 77" <<"\nChemistry= 78";
+    return 0;
+}
---
+++ 21May/findErrors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout << "Enter value: ";
+    cin >>x;
+    cout << x;
+    
+
+
+    return 0;
+}
