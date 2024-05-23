88384772f19e43fc662eb406ce5b3c91a8d20681
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-05-23 18:37:24 +0530

    23May

---
+++ 23May/areaOfRectangle.cpp
@@ -0,0 +1,11 @@
+/***C++ program to find area of Rectangle*/
+#include <iostream>
+using namespace std;
+int main(){
+    float length, breadth, area;
+    cout << "Enter length and breadth of rectangle, respectively:\n";
+    cin >> length >> breadth ;
+    area = length * breadth;
+    cout << "\nArea of the rectangle:"<< area;
+    return 0;
+}
