e504bbcc7ad0e31544067fcab332c63a53178dc9
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-05-23 18:41:28 +0530

    23 May

---
+++ 23May/areaOfSquare.cpp
@@ -0,0 +1,11 @@
+/* C++ program to find area of Square */
+#include <iostream>
+using namespace std;
+int main(){
+    float side, area;
+    cout << "Enter length of side:";
+    cin >> side;
+    area = side *side;
+    cout << "Area of Square:"<<area;
+    return 0;
+}
