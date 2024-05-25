fe4a393f66ea6d675960db879c6efcb12deec6f0
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-05-25 09:46:47 +0530

    25May

---
+++ 25May/mainCharacter.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout <<"Hello, "<<name<<"."<<"\nWelcome to Man or Gay simulator! \nRemember, in real life your choices determine who you are.";
+    return 0;
+}
